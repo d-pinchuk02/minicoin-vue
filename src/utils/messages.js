e9d406f204b3cbdc87f74df1670797ad2904256b
d-pinchuk02 <d.pinchuk02@gmail.com>
2020-05-27 00:15:57 +0300

    Add new auth messages

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,4 +1,9 @@
 export default {
   'logout': 'Вы вышли из системы',
-  'welcome': 'Добро пожаловать!'
+  'welcome': 'Добро пожаловать!',
+
+  'auth/user-not-found': 'Пользователь не найден',
+  'auth/wrong-password': 'Неверный email или пароль',
+  'auth/user-disabled': 'Пользователь заблокирован',
+  'auth/email-already-in-use': 'Такой email уже используется'
 }
