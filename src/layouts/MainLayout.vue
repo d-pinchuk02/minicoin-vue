470a6f9b44fbbfac5e5efe85189b8cf60917eae7
d-pinchuk02 <d.pinchuk02@gmail.com>
2020-05-27 03:29:38 +0300

    Add preloader to main layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,43 +1,48 @@
 <template>
-  <div>
-    <div class="app-main-layout">
+  <div >
+    <Loader v-if="loading" />
+    <div v-else>
+      <div class="app-main-layout">
+        <Navbar @click="sidebarOpen = !sidebarOpen" />
 
-      <Navbar @click="sidebarOpen = !sidebarOpen" />
+        <Sidebar v-model="sidebarOpen" />
 
-      <Sidebar v-model="sidebarOpen" />
+        <main class="app-content" :class="{full: !sidebarOpen}">
+          <div class="app-page">
+            <router-view />
+          </div>
+        </main>
 
-      <main class="app-content" :class="{full: !sidebarOpen}">
-        <div class="app-page">
-          <router-view />
+        <div class="fixed-action-btn">
+          <router-link class="btn-floating btn-large blue" to="/record">
+            <i class="large material-icons">add</i>
+          </router-link>
         </div>
-      </main>
-
-      <div class="fixed-action-btn">
-        <router-link class="btn-floating btn-large blue" to="/record">
-          <i class="large material-icons">add</i>
-        </router-link>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Navbar from '@/components/app/Navbar'
-import Sidebar from '@/components/app/Sidebar'
+import Navbar from "@/components/app/Navbar";
+import Sidebar from "@/components/app/Sidebar";
 
 export default {
-  name: 'main-layout',
+  name: "main-layout",
   data: () => ({
-    sidebarOpen: true
+    sidebarOpen: true,
+    loading: true
   }),
   async mounted() {
-    if(!Object.keys(this.$store.getters.info).length) {
-      await this.$store.dispatch('fetchInfo')
+    if (!Object.keys(this.$store.getters.info).length) {
+      await this.$store.dispatch("fetchInfo");
     }
+
+    this.loading = false;
   },
   components: {
-    Navbar, Sidebar
-  },
-
-}
+    Navbar,
+    Sidebar
+  }
+};
 </script>
