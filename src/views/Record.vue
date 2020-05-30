98c48e893566f208454f901b3512bc8336db7617
d-pinchuk02 <d.pinchuk02@gmail.com>
2020-05-31 02:34:45 +0300

    Make add record form work

--- src/views/Record.vue
+++ src/views/Record.vue
@@ -89,6 +89,7 @@
 
 <script>
 import {required, minValue} from 'vuelidate/lib/validators' 
+import {mapGetters} from 'vuex'
 
 export default {
   name: 'record',
@@ -118,6 +119,16 @@ export default {
       M.updateTextFields()
     });
   },
+  computed: {
+    ...mapGetters(['info']),
+    canCreateRecord() {
+      if (this.type === 'income') {
+        return true
+      }
+
+      return this.info.bill >= this.amount
+    }
+  },
   methods: {
     async submitHandler() {
       if (this.$v.$invalid) {
@@ -125,9 +136,30 @@ export default {
         return
       }
 
-      try {
-        
-      } catch (e) {}
+      if (this.canCreateRecord) {
+        try {
+          await this.$store.dispatch('createRecord', {
+            categoryId: this.category,
+            amount: this.amount,
+            description: this.description,
+            type: this.type,
+            date: new Date().toJSON()
+          })
+
+          const bill = this.type === 'income'
+            ? this.info.bill + this.amount
+            : this.info.bill - this.amount
+
+          await this.$store.dispatch('updateInfo', {bill})
+          this.$message('Запись успешно создана')
+          this.$v.$reset()
+          this.amount = 1
+          this.description = ''
+        } catch (e) {}
+      } else {
+        this.$message(`Недостаточно средств на счете (${this.amount - this.info.bill})`)
+      }
+
     }
   },
   destroyed() {
