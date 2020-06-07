cdd4e66ba9f7163584bf3656f6833b1d84c1fda4
d-pinchuk02 <d.pinchuk02@gmail.com>
2020-06-07 23:33:11 +0300

    Migrate empty layout to Vuetify

--- src/layouts/EmptyLayout.vue
+++ src/layouts/EmptyLayout.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="grey darken-1 empty-layout">
-    <router-view />
-  </div>
+  <v-app>
+    <v-content class="grey darken-1">
+      <v-container fluid class="fill-height">
+        <v-row align="center" justify="center">
+          <v-col cols="12" lg="5" sm="12" md="6">
+            <router-view />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
