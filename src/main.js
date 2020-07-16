caea3173ace87cc988273ddab2fe420138b098fa
d-pinchuk02 <d.pinchuk02@gmail.com>
2020-07-17 02:44:03 +0300

    Remove unnecessary code

--- src/main.js
+++ src/main.js
@@ -1,19 +1,15 @@
 import Vue from 'vue'
-import Vuelidate from 'vuelidate'
 import VueMeta from 'vue-meta'
-import Paginate from 'vuejs-paginate'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import dateFilter from '@/filters/date.filter'
 import currencyFilter from '@/filters/currency.filter'
 import localizeFilter from '@/filters/localize.filter'
-import tooltipDirective from '@/directives/tooltip.directive'
 import messagePlugin from '@/utils/message.plugin'
 import titlePlugin from '@/utils/title.plugin'
 import Loader from '@/components/app/Loader'
 import './registerServiceWorker'
-//import 'materialize-css/dist/js/materialize.min'
 
 import firebase from 'firebase/app'
 import 'firebase/auth'
@@ -24,14 +20,11 @@ Vue.config.productionTip = false
 
 Vue.use(messagePlugin)
 Vue.use(titlePlugin)
-Vue.use(Vuelidate)
 Vue.use(VueMeta)
 Vue.filter('date', dateFilter)
 Vue.filter('currency', currencyFilter)
 Vue.filter('localize', localizeFilter)
-Vue.directive('tooltip', tooltipDirective)
 Vue.component('Loader', Loader)
-Vue.component('Paginate', Paginate)
 
 const firebaseConfig = {
   apiKey:            process.env.VUE_APP_FIRE_API_KEY,
